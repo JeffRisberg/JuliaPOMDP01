2c3772827a92646600353ff595f52b55b4d4fb9b
JeffRisberg <j.s.risberg@gmail.com>
2021-06-03 21:39:59 -0700

    set up example

--- src/Example01.jl
+++ src/Example01.jl
@@ -2,192 +2,57 @@
 MDP:
 - Julia version: 1.6.0
 - Author: jeff
-- Date: 2021-05-06
+- Date: 2021-06-01
 =#
-include("common_defs.jl")
-include("common_checks.jl")
-include("valueiteration.jl")
+using Pkg; Pkg.add("POMDPs"); Pkg.add("QMDP")
+using Pkg; Pkg.add("QuickPOMDPs"); Pkg.add("POMDPModelTools"); Pkg.add("POMDPSimulators")
 
-const MinX = 0
-const MaxX = 100
-const StepX = 20
+using POMDPs, QMDP, QuickPOMDPs, POMDPModelTools, POMDPSimulators
 
-# mdp definition
-mdp = MDP()
+m = QuickPOMDP(
+    states = ["left", "right"],
+    actions = ["left", "right", "listen"],
+    observations = ["left", "right"],
+    initialstate = Uniform(["left", "right"]),
+    discount = 0.95,
 
-# state space
-statevariable!(mdp, "x", MinX, MaxX)  # continuous
-statevariable!(mdp, "goal", ["no", "yes"])  # discrete
-
-# action space
-actionvariable!(mdp, "move", ["W", "E", "stop"])  # discrete
-
-function isgoal(x::Float64)
-  if abs(x - MaxX / 2) < StepX
-    return "yes"
-  else
-    return "no"
-  end
-end
-
-function mytransition(x::Float64, goal::AbstractString, move::AbstractString)
-  if isgoal(x) == "yes" && goal == "yes"
-    return [([x, isgoal(x)], 1.0)]
-  end
-
-  if move == "E"
-    if x >= MaxX
-      return [
-        ([x, isgoal(x)], 0.9),
-        ([x - StepX, isgoal(x - StepX)], 0.1)]
-    elseif x <= MinX
-      return [
-        ([x, isgoal(x)], 0.2),
-        ([x + StepX, isgoal(x + StepX)], 0.8)]
-    else
-      return [
-        ([x, isgoal(x)], 0.1),
-        ([x - StepX, isgoal(x - StepX)], 0.1),
-        ([x + StepX, isgoal(x + StepX)], 0.8)]
-    end
-  elseif move == "W"
-    if x >= MaxX
-      return [
-        ([x, isgoal(x)], 0.1),
-        ([x - StepX, isgoal(x - StepX)], 0.9)]
-    elseif x <= MinX
-      return [
-        ([x, isgoal(x)], 0.9),
-        ([x + StepX, isgoal(x + StepX)], 0.1)]
-    else
-      return [
-        ([x, isgoal(x)], 0.1),
-        ([x - StepX, isgoal(x - StepX)], 0.8),
-        ([x + StepX, isgoal(x + StepX)], 0.1)]
-    end
-  elseif move == "stop"
-    return [([x, isgoal(x)], 1.0)]
-  end
-end
-
-transition!(mdp, ["x", "goal", "move"], mytransition)
-
-function myreward(x::Float64, goal::AbstractString, move::AbstractString)
-  if goal == "yes" && move == "stop"
-    return 1
-  else
-    return 0
-  end
-end
-
-reward!(mdp, ["x", "goal", "move"], myreward)
-
-solver = SerialValueIteration()
-
-const StepX = 20
-discretize_statevariable!(solver, "x", StepX)
-
-lazyCheck(mdp, solver)
-
-statespace, actionspace = getspaces(mdp, solver)
-solver.stategrid = RectangleGrid(statespace...)
-println(solver.stategrid)
-solver.actiongrid = RectangleGrid(actionspace...)
-println(solver.actiongrid)
-
-println(length(mdp.transition.argnames) == length(mdp.statemap) + length(mdp.actionmap))
-
-function solveset(mdp::MDP, svi::SerialValueIteration)
-
-  statedim = length(mdp.statemap)
-  stateargs = mdp.reward.argnames[1:statedim]
-  actionargs = mdp.reward.argnames[1 + statedim:end]
-
-  nstates = length(svi.stategrid)
-  nactions = length(svi.actiongrid)
-
-  vold = zeros(nstates)
-  vnew = zeros(nstates)
-  qval = zeros(nactions, nstates)
-  resid = 0.0
-
-  iter = 0
-  itertime = 0.0
-  cputime = 0.0
-
-  for i in 1:svi.maxiter
-
-    #tic()
-    resid = -Inf
-
-    for istate in 1:nstates
-
-      state = getvar(svi.stategrid, mdp.statemap, stateargs, istate)
-      qhi = -Inf
-
-      for iaction in 1:nactions
-
-        action = getvar(svi.actiongrid, mdp.actionmap, actionargs, iaction)
-
-        statepIdxs, probs = transition(mdp, svi, state, action, stateargs)
-        qnow = 0.0
-
-        for istatep in 1:length(statepIdxs)
-          qnow += probs[istatep] * vold[statepIdxs[istatep]]
+    transition = function (s, a)
+        if a == "listen"
+            return Deterministic(s) # tiger stays behind the same door
+        else # a door is opened
+            return Uniform(["left", "right"]) # reset
         end
+    end,
 
-        qnow *= svi.discount
-        qnow += reward(mdp, state, action)
-
-        qval[iaction, istate] = qnow
-
-        if qnow > qhi
-          qhi = qnow
-          vnew[istate] = qhi
+    observation = function (s, a, sp)
+        if a == "listen"
+            if sp == "left"
+                return SparseCat(["left", "right"], [0.85, 0.15]) # sparse categorical distribution
+            else
+                return SparseCat(["right", "left"], [0.85, 0.15])
+            end
+        else
+            return Uniform(["left", "right"])
         end
+    end,
 
-      end
-
-      # use infinity-norm
-      newresid = (vold[istate] - vnew[istate])^2
-      newresid > resid ? resid = newresid : nothing
-
+    reward = function (s, a)
+        if a == "listen"
+            return -1.0
+        elseif s == a # the tiger was found
+            return -100.0
+        else # the tiger was escaped
+            return 10.0
+        end
     end
+)
 
-    #itertime = toq()
-    #cputime += itertime
+solver = QMDPSolver()
+policy = solve(solver, m)
 
-    if svi.verbose
-      println(string("iter $iter, resid: $resid"))
-    end
-
-    resid < svi.tol ? break : nothing
-
-    vtmp = vold
-    vold = vnew
-    vnew = vtmp
-    iter = i
-
-  end
-
-  if iter == svi.maxiter
-    @warn(string(
-      "maximum number of iterations reached; check accuracy of solutions"))
-  end
-
-  @info(string(
-    "value iteration solution generated\n",
-    "cputime [s] = ", cputime, "\n",
-    "number of iterations = ", iter, "\n",
-    "residual = ", resid))
-
-  return ValueIterationSolution(
-    qval,
-    svi.stategrid,
-    svi.actiongrid,
-    cputime,
-    iter,
-    resid)
+rsum = 0.0
+for (s,b,a,o,r) in stepthrough(m, policy, "s,b,a,o,r", max_steps=10)
+    println("s: $s, b: $([pdf(b,s) for s in states(m)]), a: $a, o: $o")
+    global rsum += r
 end
-
-solveset(mdp, solver)
+println("Undiscounted reward was $rsum.")
